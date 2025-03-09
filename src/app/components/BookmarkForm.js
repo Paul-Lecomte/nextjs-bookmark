977e80dc7a11500613e5f8083a34822835bbfea1
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-09 09:44:59 +0100

    added the component

---
+++ src/app/components/BookmarkForm.js
@@ -0,0 +1,10 @@
+export default function BookmarkForm({ onAdd }) {
+    return (
+        <div className="mb-4">
+            <input type="text" placeholder="Title" className="border p-2 mr-2 rounded" />
+            <input type="url" placeholder="URL" className="border p-2 mr-2 rounded" />
+            <input type="text" placeholder="Category" className="border p-2 mr-2 rounded" />
+            <button className="bg-blue-500 text-white p-2 rounded">Add</button>
+        </div>
+    );
+}
