2778c520f0041506740cf6b0c0aeb775fc007e07
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-09 10:38:54 +0100

    changed the export folder

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export', // Export as static files
     trailingSlash: true, // Ensure trailing slash for Chrome extensions
+    distDir: 'build',  // Custom directory to store the build files
 };
 
 module.exports = nextConfig;
