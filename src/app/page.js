c8f3ca96b4eba8cae51bea9ddf746df92e0f1c7e
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-09 09:47:19 +0100

    inserted the bookform

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import BookmarkForm from "./components/BookmarkForm";
 
 export default function Home() {
   return (
-    <div></div>
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Bookmark Manager</h1>
+        <BookmarkForm/>
+      </div>
   );
 }
